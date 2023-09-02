057d0b7f615572a08afbc88475421198a95c9b62
Aryan Patidar <101715371+Aryan568@users.noreply.github.com>
2023-09-03 01:40:54 +0530

    Update ApiInterface.java

--- app/src/main/java/com/example/whatsappclone/API/ApiInterface.java
+++ app/src/main/java/com/example/whatsappclone/API/ApiInterface.java
@@ -7,7 +7,7 @@ import retrofit2.http.POST;
 
 public interface ApiInterface {
 
-    @Headers({"Authorization: key=" + "AAAADuyH4CE:APA91bF9dOMcLErwjc6E0Mqhudsjo6EvelmDpz9E0YMZtUzOnuR5bIZiTvV2_5e2o9SGpN_4wxBM4cSrAnIvoJYQ13ApeDhRZW-_9mXtKhMbNvm2UuOut2nC3V3pmu5u_hThcoke9qlU", "Content-Type:application/json"})
+    @Headers({"Authorization: key=" + ""})
     @POST("fcm/send")
     Call<Notification> sendNotification(@Body Notification notification);
 

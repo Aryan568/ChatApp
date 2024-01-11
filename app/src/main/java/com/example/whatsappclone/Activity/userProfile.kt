985b860a847e1990e65bf7ff31b59a96c0f9e2ce
Aryan568 <aryanpatidar730@gmail.com>
2024-01-11 10:55:30 +0530

    Android

---
+++ app/src/main/java/com/example/whatsappclone/Activity/userProfile.kt
@@ -0,0 +1,12 @@
+package com.example.whatsappclone.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.whatsappclone.R
+
+class userProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
